2822afa05a0b700ffbb56ea3fd08b7fac0d798c8
mhuertas <manuel.huertas.lopez@gmail.com>
2022-08-22 19:20:13 +0000

    Files to make packing

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# MonitorManager WebReportApi Python
+# MonitorManager https://towardsdatascience.com/create-your-own-python-package-and-publish-it-into-pypi-9306a29bc116****WebReportApi Python
 
 Monitor Manager Web Report Api in Python
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='mmwebreport',
+    version='0.0.1',
+    author='Manuel Huertas Lopez',
+    description='Monitor Manager Web Rerpot API',
+    packages=['mmwebreport','mmwebreport.core','mmwebreport.retrieve']
+)
