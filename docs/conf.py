b31a383db63f9b6171f3c84942472c127c8e414f
mhuertas <manuel.huertas.lopez@gmail.com>
2022-09-02 09:43:38 +0000

    workflow to generate doc

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,29 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 import sphinx_rtd_theme
+
+import sys, os
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
 
 project = 'mmwebreportapi'
 copyright = '2022, Manuel Huertas Lopez'
 author = 'Manuel Huertas Lopez'
+
 release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
@@ -21,7 +39,6 @@ autodoc_mock_imports = ["external_library"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 
 # -- Options for HTML output -------------------------------------------------
